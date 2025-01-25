0109da36979d9c06cb48a2bce13209b0e24633dc
Robert Wąchal <robert.wachal@tlrw.studio>
2025-01-25 21:45:19 +0100

    Save

---
+++ src/pg/envs.bash
@@ -0,0 +1,11 @@
+# shellcheck shell=bash
+
+PGHOST="${PGHOSTADDR:-${PGHOST:-127.0.0.1}}"
+PGPORT="${PGPORT:-5432}"
+PGUSER="${PGUSER:-${POSTGRES_USER:-postgres}}"
+
+export PGHOST PGPORT PGUSER
+
+PGPASSWORD="${PGPASSWORD:-${POSTGRES_PASSWORD:-}}" && {
+    [[ -n ${PGPASSWORD} ]] && export PGPASSWORD
+}
